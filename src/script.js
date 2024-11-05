3c5b800cd8467edd9d0411dc36f1f750b9816fc8
wendy <chamunorwawendy@gmail.com>
2024-11-05 08:04:14 +0200

    coded search engine in js

---
+++ src/script.js
@@ -0,0 +1,11 @@
+function submitButton(event){
+    event.preventDefault();
+    let searchInput = document.querySelector("#search-form-input");
+    let cityElement = document.querySelector("#city-name");
+    cityElement.innerHTML = searchInput.value;
+}
+
+
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", submitButton);
